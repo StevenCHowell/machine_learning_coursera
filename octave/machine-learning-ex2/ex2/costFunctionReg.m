7d79cebfb5b42a14cc5787006da0013379f606ba
Steven C. Howell <steven.c.howell@zoho.com>
2017-06-03 19:10:58 -0400

    implement regularized cost function

--- octave/machine-learning-ex2/ex2/costFunctionReg.m
+++ octave/machine-learning-ex2/ex2/costFunctionReg.m
@@ -2,14 +2,14 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+% You need to return the following variables correctly
+% J = 0;
+% grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -17,10 +17,16 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X * theta);
 
+% calculate the cost function
+s1 = sum(-y .* log(h) - (1 - y) .* log(1 - h));
+J = s1 / m  + lambda * theta(2:end)' * theta(2:end) / (2 * m);
 
-
-
+% calculate the gradient
+reg = lambda * theta / m;
+reg(1) = 0;
+grad = (h - y)' * X / m + reg';
 
 % =============================================================
 
