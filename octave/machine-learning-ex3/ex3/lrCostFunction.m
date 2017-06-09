bc063089588b5ea64f63b1bbf870179cb00cb657
Steven C. Howell <steven.c.howell@zoho.com>
2017-06-09 07:57:24 -0400

    implement vectorized code for calculating the regularized logistic regression cost function

--- octave/machine-learning-ex3/ex3/lrCostFunction.m
+++ octave/machine-learning-ex3/ex3/lrCostFunction.m
@@ -1,16 +1,16 @@
 function [J, grad] = lrCostFunction(theta, X, y, lambda)
-%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with
 %regularization
 %   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+% You need to return the following variables correctly
+% J = 0;
+% grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -25,25 +25,27 @@ grad = zeros(size(theta));
 %
 %       Each row of the resulting matrix will contain the value of the
 %       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
+%       the cost function and gradient computations.
 %
-% Hint: When computing the gradient of the regularized cost function, 
+% Hint: When computing the gradient of the regularized cost function,
 %       there're many possible vectorized solutions, but one solution
 %       looks like:
 %           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           temp = theta;
+%           temp(1) = 0;   % because we don't add anything for j = 0
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X * theta);
 
+% calculate the cost function
+s1 = sum(-y .* log(h) - (1 - y) .* log(1 - h));
+J = s1 / m  + lambda * theta(2:end)' * theta(2:end) / (2 * m);
 
-
-
-
-
-
-
+% calculate the gradient
+reg = lambda * theta / m;
+reg(1) = 0;
+grad = (h - y)' * X / m + reg';
 
 % =============================================================
 
