26427b660d2ae0a66dea52b28527976a04c9a1b6
Steven C. Howell <steven.c.howell@zoho.com>
2017-06-09 09:36:02 -0400

    implement feedforward propagation for neural network

--- octave/machine-learning-ex3/ex3/predict.m
+++ octave/machine-learning-ex3/ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,14 +21,19 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% append 1 to the input
+a1 = [ones(m, 1) X];
 
+% compute the hidden layer
+z2 = Theta1 * a1';
+a2 = [ones(m, 1) sigmoid(z2)'];
 
+% compute the output layer
+z3 = Theta2 * a2';
+a3 = sigmoid(z3);
 
-
-
-
-
-
+% compute the predicions
+[max_vals, p(:)] = max(a3);
 % =========================================================================
 
 
