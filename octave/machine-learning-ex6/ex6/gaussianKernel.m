8940bb29e9b0230338c3d4a71bc23f3e3ee50d00
Steven C. Howell <steven.c.howell@zoho.com>
2017-07-01 06:41:16 -0400

    implement gaussian kernel calculation

--- octave/machine-learning-ex6/ex6/gaussianKernel.m
+++ octave/machine-learning-ex6/ex6/gaussianKernel.m
@@ -7,7 +7,7 @@ function sim = gaussianKernel(x1, x2, sigma)
 x1 = x1(:); x2 = x2(:);
 
 % You need to return the following variables correctly.
-sim = 0;
+% sim = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the similarity between x1
@@ -16,11 +16,9 @@ sim = 0;
 %
 %
 
-
-
-
-
+diff = x1 - x2;
+sim = exp(-diff' * diff / (2 * sigma^2));
 
 % =============================================================
-    
+
 end
