319787714fba3c3a0bc696620541e6c780cd9631
Steven C. Howell <steven.c.howell@zoho.com>
2017-07-01 07:10:48 -0400

    add loop to evaluate different parameter sets

--- octave/machine-learning-ex6/ex6/dataset3Params.m
+++ octave/machine-learning-ex6/ex6/dataset3Params.m
@@ -2,32 +2,46 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+% C = 1;
+% sigma = 0.3;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+vals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+last_error = 9999;
+for i = 1:numel(vals)
+    C = vals(i);
+    for j = 1:numel(vals)
+        sigma = vals(j);
+        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        predictions = svmPredict(model, Xval);
+        p_error = mean(double(predictions ~= yval));
+        if p_error < last_error:
+            last_error = p_error;
+            C_best = C;
+            sigma_best = sigma;
+        end
+    end
+end
 
-
-
-
-
+C = C_best;
+sigma = sigma_best;
 
 % =========================================================================
 
