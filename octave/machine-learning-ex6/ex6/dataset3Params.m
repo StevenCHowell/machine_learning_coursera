f147a5f0ea5f9e12fd93b5d0fea4c0afdfe8875f
Steven C. Howell <steven.c.howell@zoho.com>
2017-07-01 07:41:02 -0400

    identify best C and sigma value

--- octave/machine-learning-ex6/ex6/dataset3Params.m
+++ octave/machine-learning-ex6/ex6/dataset3Params.m
@@ -22,27 +22,31 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
+C = 1;
+sigma = 0.1;
 
-vals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
-last_error = 9999;
-for i = 1:numel(vals)
-    C = vals(i);
-    for j = 1:numel(vals)
-        sigma = vals(j);
-        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
-        predictions = svmPredict(model, Xval);
-        p_error = mean(double(predictions ~= yval));
-        if p_error < last_error:
-            last_error = p_error;
-            C_best = C;
-            sigma_best = sigma;
+# useful for trying different values of C and sigma
+if ~exist('C') && ~exist('sigma')
+    vals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+    last_error = 9999;
+    for i = 1:numel(vals)
+        C = vals(i);
+        for j = 1:numel(vals)
+            sigma = vals(j);
+            model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+            predictions = svmPredict(model, Xval);
+            p_error = mean(double(predictions ~= yval));
+            if p_error < last_error
+                last_error = p_error;
+                C_best = C;
+                sigma_best = sigma;
+            end
         end
     end
+
+    C = C_best;
+    sigma = sigma_best;
 end
-
-C = C_best;
-sigma = sigma_best;
-
 % =========================================================================
 
 end
