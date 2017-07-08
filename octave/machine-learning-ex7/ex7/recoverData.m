71610ac1a2dd821096af5869223a660b6efa33c8
Steven C. Howell <steven.c.howell@zoho.com>
2017-07-08 08:19:49 -0400

    implement projection of reduced data back onto original data space

--- octave/machine-learning-ex7/ex7/recoverData.m
+++ octave/machine-learning-ex7/ex7/recoverData.m
@@ -21,7 +21,9 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
-
+for j = 1:length(U)
+  X_rec(:, j) = Z * U(j, 1:K)';
+end
 
 % =============================================================
 
