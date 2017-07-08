59a32a4ecf6880b55c8320d0bc746589c9de37ec
Steven C. Howell <steven.c.howell@zoho.com>
2017-07-08 07:47:10 -0400

    implement random centroid positions

--- octave/machine-learning-ex7/ex7/kMeansInitCentroids.m
+++ octave/machine-learning-ex7/ex7/kMeansInitCentroids.m
@@ -1,26 +1,22 @@
 function centroids = kMeansInitCentroids(X, K)
-%KMEANSINITCENTROIDS This function initializes K centroids that are to be 
+%KMEANSINITCENTROIDS This function initializes K centroids that are to be
 %used in K-Means on the dataset X
 %   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
 %   used with the K-Means on the dataset X
 %
 
 % You should return this values correctly
-centroids = zeros(K, size(X, 2));
+% centroids = zeros(K, size(X, 2));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should set centroids to randomly chosen examples from
 %               the dataset X
 %
 
-
-
-
-
-
-
+n = length(X);
+idx = randperm(n, K);
+centroids = X(idx, :);
 
 % =============================================================
 
 end
-
