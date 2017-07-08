2aa21c1a004188f29ffd9fec520c50bc41095cef
Steven C. Howell <steven.c.howell@zoho.com>
2017-07-08 07:34:37 -0400

    implement vectorized averaging of points in a cluster

--- octave/machine-learning-ex7/ex7/computeCentroids.m
+++ octave/machine-learning-ex7/ex7/computeCentroids.m
@@ -1,7 +1,7 @@
 function centroids = computeCentroids(X, idx, K)
-%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%COMPUTECENTROIDS returns the new centroids by computing the means of the
 %data points assigned to each centroid.
-%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by
 %   computing the means of the data points assigned to each centroid. It is
 %   given a dataset X where each row is a single data point, a vector
 %   idx of centroid assignments (i.e. each entry in range [1..K]) for each
@@ -24,17 +24,11 @@ centroids = zeros(K, n);
 %               centroid i.
 %
 % Note: You can use a for-loop over the centroids to compute this.
-%
 
-
-
-
-
-
-
+for i=1:K
+  centroids(i, :) = mean(X(idx == i, :), 1);
+end
 
 % =============================================================
 
-
 end
-
