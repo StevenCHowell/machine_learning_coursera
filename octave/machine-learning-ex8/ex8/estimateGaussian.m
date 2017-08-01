3cdc73f6485f7d2652edc7efdd4bf546cc5de419
Steven C. Howell <steven.howell@nist.gov>
2017-07-31 23:07:35 -0400

    vectorized mean and var of data

--- octave/machine-learning-ex8/ex8/estimateGaussian.m
+++ octave/machine-learning-ex8/ex8/estimateGaussian.m
@@ -1,18 +1,18 @@
 function [mu sigma2] = estimateGaussian(X)
-%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%ESTIMATEGAUSSIAN This function estimates the parameters of a
 %Gaussian distribution using the data in X
-%   [mu sigma2] = estimateGaussian(X), 
+%   [mu sigma2] = estimateGaussian(X),
 %   The input X is the dataset with each n-dimensional data point in one row
 %   The output is an n-dimensional vector mu, the mean of the data set
 %   and the variances sigma^2, an n x 1 vector
-% 
+%
 
 % Useful variables
 [m, n] = size(X);
 
 % You should return these values correctly
-mu = zeros(n, 1);
-sigma2 = zeros(n, 1);
+% mu = zeros(n, 1);
+% sigma2 = zeros(n, 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the mean of the data and the variances
@@ -21,16 +21,9 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
-
-
-
-
-
-
-
-
+mu = mean(X);
+sigma2 = var(X, 1);
 
 % =============================================================
-
 
 end
